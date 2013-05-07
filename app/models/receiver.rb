5240e2144efd2d16c55e377ad3de16ca9453a746
Simon Escobar <s.escob.b.27@gmail.com>
2013-05-07 14:00:11 -0500

    new receiver logic

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -9,12 +9,22 @@ class Receiver < ActiveRecord::Base
 
   def self.add_to_database(req_params)
   	gcm_device = Gcm::Device.new registration_id: req_params['regId']
-  	gcm_device.save
 
     user = User.new name: req_params['name'], email: req_params['email']
     user.birthday = req_params['birthday'] unless req_params['birthday'].empty?
 
+    # en los param viene un atributo likes que son unos strings concatenados con ','
+    # que hacen referencia a las entidades que le gustan al usuario
+    likes = req_params['likes'].to_s.split(',').each do |like|
+      like.strip!
+    end
+    # asignar entidades (restaurantes, bares o discotecas) al usuario
+    user.entities = Entity.where entity_name: likes
+
+    # si el usuario es agregado satisfactoriamente tambien guardar el gcm::device
+    # y el receiver
     if user.save
+      gcm_device.save
       Receiver.create gcm_device_id: gcm_device.id, user: user
     end
 
