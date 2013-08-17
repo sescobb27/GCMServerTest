7ffaf561e4a4c376e8dcf9109945e3bd05b80cae
Simon Ecobar Benitez <s.escob.b.27@gmail.com>
2013-08-16 23:32:13 -0500

    user specification with factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user do
+  factory :user do |user|
+    user.name ''
+    user.email ''
+    user.birthday ''
+    user.gcm_device_id ''
+    user.entities ''
+    user.secure_token ''
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context "should be invalid users without" do
+        it "name"
+        it "email"
+        it "birthday"
+        it "gcm_device_id"
+        it "likes"
+        it "unique email"
+        it "valid email format"
+        it "valid name's length"
+    end
+
+    context "should be valid users because" do
+        it "has valid name"
+        it "has valid email"
+        it "has valid birthday"
+        it "has gcm_device_id"
+        it "has likes"
+        it "has a unique email"
+        it "has a valid email format"
+        it "has a valid name's length"
+    end
 end
