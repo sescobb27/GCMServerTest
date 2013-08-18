8f67b1cd101aabfc2adc52471459b5db663b1a15
Simon Ecobar Benitez <s.escob.b.27@gmail.com>
2013-08-18 03:52:52 -0500

    there were causing problems with the schema, and there were not longer needed

--- db/migrate/20130420160844_add_attr_to_receiver.rb
+++
@@ -1,6 +0,0 @@
-class AddAttrToReceiver < ActiveRecord::Migration
-  def change
-    add_column :receivers, :name, :string, :null => false
-    add_column :receivers, :email, :string,:null => false
-  end
-end
--- db/migrate/20130421084019_add_reg_id_to_receiver.rb
+++
@@ -1,5 +0,0 @@
-class AddRegIdToReceiver < ActiveRecord::Migration
-  def change
-    add_column :receivers, :gcm_device_id, :integer, :null => false
-  end
-end
